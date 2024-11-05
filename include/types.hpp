1d95fc5c43d0b9dc4a17268ccec005b9e6ce66c5
Tom Schammo <tom@tomschammo.com>
2024-11-05 14:50:56 +0100

    feat: Add header file with type aliases

---
+++ include/types.hpp
@@ -0,0 +1,34 @@
+
+#ifndef TYPES_HPP
+#define TYPES_HPP
+
+#include <cstddef>
+#include <cstdint>
+
+namespace utilix {
+
+typedef std::uint8_t u8;
+typedef std::int8_t i8;
+
+typedef std::uint16_t u16;
+typedef std::int16_t i16;
+
+typedef std::uint32_t u32;
+typedef std::int32_t i32;
+
+typedef std::uint64_t u64;
+typedef std::int64_t i64;
+
+typedef std::size_t size;
+
+#ifndef NO_FLOATS
+static_assert(sizeof(float) == 4, "Expected float to be 32 bits wide!");
+static_assert(sizeof(double) == 8, "Expected double to be 64 bits wide!");
+
+typedef float f32;
+typedef double f64;
+#endif
+
+} // namespace utilix
+
+#endif // !TYPES_HPP
