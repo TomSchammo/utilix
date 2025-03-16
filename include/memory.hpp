477790953acace64cb51ffa61a1ec6374ce97cba
Tom Schammo <tom@tomschammo.com>
2025-03-16 11:46:39 +0100

    feat: Add easy memory unit conversion function

---
+++ include/memory.hpp
@@ -0,0 +1,31 @@
+#ifndef MEMORY_HPP
+#define MEMORY_HPP
+
+[[nodiscard]] constexpr unsigned long long kilo_bytes(unsigned long long n) {
+  return n * 1024ULL;
+}
+
+template <unsigned long long n>
+[[nodiscard]] constexpr unsigned long long kilo_bytes() {
+  return n * 1024ULL;
+}
+
+[[nodiscard]] constexpr unsigned long long mega_bytes(unsigned long long n) {
+  return n * 1024ULL * 1024ULL;
+}
+
+template <unsigned long long n>
+[[nodiscard]] constexpr unsigned long long mega_bytes() {
+  return n * 1024ULL * 1024ULL;
+}
+
+[[nodiscard]] constexpr unsigned long long giga_bytes(unsigned long long n) {
+  return n * 1024ULL * 1024ULL * 1024ULL;
+}
+
+template <unsigned long long n>
+[[nodiscard]] constexpr unsigned long long giga_bytes() {
+  return n * 1024ULL * 1024ULL * 1024ULL;
+}
+
+#endif // MEMORY_HPP
