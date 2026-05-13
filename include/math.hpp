24d47decc71095e3a244a161aee49cf00b62c6fb
Tom Schammo <tom@tomschammo.com>
2026-05-13 20:07:59 +0200

    fix: Fix bugs and use concepts instead of `static_assert`

--- include/math.hpp
+++ include/math.hpp
@@ -9,10 +9,12 @@ concept NumericInt = std::is_integral_v<T> && !std::is_same_v<T, bool>;
 template <typename T>
 concept UnsignedInt = NumericInt<T> && std::is_unsigned_v<T>;
 
-template <NumericInt A, UnsignedInt B> constexpr B euc_mod(A a, B b) {
-  static_assert(sizeof(B) >= sizeof(A), "b must be at least as wide as a");
+template <NumericInt A, UnsignedInt B>
+constexpr B euc_mod(A a, B b)
+  requires(sizeof(B) >= sizeof(A))
+{
   B r = static_cast<B>(a) % b;
-  return (a < A{0}) ? b - r : r;
+  return (a < A{0} and r != 0) ? b - r : r;
 }
 
 #endif // UTILIX_MATH_HPP
